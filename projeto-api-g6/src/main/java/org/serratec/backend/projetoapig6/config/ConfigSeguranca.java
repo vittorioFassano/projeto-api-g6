74aab7aafe7df6f919b539e6c9dd6c0e2dec2a17
vittorio <vittfassano@gmail.com>
2024-05-22 15:04:50 -0300

    feat: criando security package

---
+++ projeto-api-g6/src/main/java/org/serratec/backend/projetoapig6/config/ConfigSeguranca.java
@@ -0,0 +1,85 @@
+package org.serratec.backend.projetoapig6.config;
+
+import java.util.Arrays;
+
+
+
+
+import org.serratec.backend.servicedto.security.JwtAuthenticationFilter;
+import org.serratec.backend.servicedto.security.JwtAuthorizationFilter;
+import org.serratec.backend.servicedto.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigSeguranca {
+	
+	@Autowired
+	UserDetailsService userDetailsService;
+	
+	@Autowired
+	JwtUtil jwtUtil;
+	
+	@Bean
+	 SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable())
+		.cors((cors) -> cors.configurationSource(corsConfigurationSource()))
+		.httpBasic(Customizer.withDefaults()).authorizeHttpRequests(requests -> {
+			requests.requestMatchers(HttpMethod.GET, "/login").permitAll();
+			requests.requestMatchers(HttpMethod.GET, "/enderecos/**").permitAll();
+			requests.requestMatchers(HttpMethod.GET, "/usuarios").hasAnyAuthority("ADMIN", "USER");
+			requests.requestMatchers(HttpMethod.POST, "/usuarios").hasAuthority("ADMIN")
+			.anyRequest().authenticated();
+		}).sessionManagement(session -> {
+			session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		});
+		
+		JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(
+				authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)), jwtUtil);
+		jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+		
+		http.addFilter(jwtAuthenticationFilter);
+		http.addFilter(new JwtAuthorizationFilter(
+				authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)), jwtUtil, userDetailsService));
+		
+		return http.build();
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration.applyPermitDefaultValues());
+		
+		return source;
+	}
+	
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
