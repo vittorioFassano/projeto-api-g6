3357d7b5180a4c06e32d14930eda691d672609ee
vittorio <vittfassano@gmail.com>
2024-05-22 11:37:30 -0300

    feat: criando projeto base

---
+++ projeto-api-g6/src/main/java/org/serratec/backend/projetoapig6/ProjetoApiG6Application.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.projetoapig6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiG6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiG6Application.class, args);
+	}
+
+}
---
+++ projeto-api-g6/src/test/java/org/serratec/backend/projetoapig6/ProjetoApiG6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.projetoapig6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiG6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
